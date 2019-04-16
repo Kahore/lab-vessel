a056e8230c1bda4d62f8059a4f0aaa85a6a3135a
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-04-16 18:23:45 +0700

    refactoring history style

--- src/components/Vesselnfo/History.vue
+++ src/components/Vesselnfo/History.vue
@@ -1,37 +1,34 @@
 <template>
   <section>
-    <div class="riContainer">
+    <div class="history-block">
       <h2>История по сосуду</h2>
-      <div class="historyContainer">
-        <div class="historyTableHeaderRow">
-          <div class="historyTable">Параметр</div>
-          <div class="historyTable">Значение</div>
-          <div class="historyTable">Изменения</div>
+      <div class="history-container">
+        <div class="history-table__header-row">
+          <div class="history-table">Параметр</div>
+          <div class="history-table">Значение</div>
+          <div class="history-table">Изменения</div>
         </div>
-        <!-- historyTableHeaderRow -->
-
-        <!--  <div id="historyTableRowContainer" > -->
+        <!-- .history-table__header-row -->
         <div>
-          <!-- <div class="awaitLoad" id="myAwaitLoad" v-show="loading" > Some text </div> -->
           <div
-            class="historyTableRow"
+            class="history-table__row"
             :id="history.ID"
             v-for="history in histories"
             :key="history.ID"
           >
-            <div class="historyTable">{{ history.Item }}</div>
-            <div class="historyTable">{{ history.ItemVal }}</div>
-            <div class="historyTable">
+            <div class="history-table">{{ history.Item }}</div>
+            <div class="history-table">{{ history.ItemVal }}</div>
+            <div class="history-table">
               <p>{{ history.CreatedBy }}</p>
               <p>{{ history.Created }}</p>
             </div>
           </div>
-          <!-- historyTableRow -->
+          <!-- .history-table__row -->
         </div>
       </div>
-      <!-- historyContainer -->
+      <!-- .history-container -->
     </div>
-    <!-- riContainer -->
+    <!-- .history-block -->
   </section>
 </template>
 
