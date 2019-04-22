7061ed61b5938e1723002e9aceca97b774edce8e
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-04-22 12:48:05 +0700

    hotfix break comp

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,20 +11,21 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    'plugin:vue/essential'
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/babel',
-    'prettier/flowtype',
-    'prettier/react',
-    'prettier/standard',
-    'prettier/unicorn',
-    'prettier/vue'
+    // 'standard',
+    // 'prettier'
+    // 'prettier/@typescript-eslint',
+    // 'prettier/babel',
+    // 'prettier/flowtype',
+    // 'prettier/react',
+    // 'prettier/standard',
+    // 'prettier/unicorn',
+    // 'prettier/vue'
   ],
   // required to lint *.vue files
-  plugins: ['prettier', 'vue'],
+  // plugins: ['prettier', 'vue'],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     semi: ['error', 'always'],
@@ -32,7 +33,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 'error'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    //'prettier/prettier': 'error'
   }
 };
