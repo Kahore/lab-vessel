c52915d20c1b20eeb438bc98b41cab332d374291
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-05-01 15:26:53 +0700

    Pass field vessel data in own object

--- src/Query/VesselFieldFiller.sql
+++ src/Query/VesselFieldFiller.sql
@@ -15,8 +15,8 @@ BEGIN
   WHERE ID = '@unid@' )
 
 	SELECT [LabProtocols].dbo.qfn_XmlToJson ((
-	  
-	  SELECT /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@ @UserName@,@GetDate@*/ ELE.[Status]
+	  SELECT
+      (   SELECT /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@ @UserName@,@GetDate@*/ ELE.[Status]
 	  				  , ELE.[ID]
 					  , CASE WHEN ELE.[Condition] IS NULL THEN '' ELSE ELE.[Condition] END AS Condition
 			   		  , CASE WHEN ELE.[CommissioningDate]  IS NULL THEN '' ELSE  convert(char(10),ELE.[CommissioningDate],103) END AS CommissioningDate 
@@ -27,10 +27,12 @@ BEGIN
 			   		  , CASE WHEN ELE.[Location] IS NULL THEN '' ELSE ELE.[Location] END AS Location
               , CASE WHEN ELE.[VesselType] IS NULL THEN '' ELSE ELE.[VesselType] END AS [VesselType]
 	   				  , CASE WHEN @BranchCodeCheck in (SELECT nstr
-      FROM [NKReports].[dbo].[Params_To_Table] (@AccessList, @delimiter)) THEN @btnSave ELSE @No_btnSave END AS [Btn_Save]
+        FROM [NKReports].[dbo].[Params_To_Table] (@AccessList, @delimiter)) THEN @btnSave ELSE @No_btnSave END AS [Btn_Save]
 	   				  , CASE WHEN @BranchCodeCheck in (SELECT nstr
-      FROM [NKReports].[dbo].[Params_To_Table] (@AccessList, @delimiter)) THEN 'true' ELSE 'false' END AS [CanIEditVessel]
+        FROM [NKReports].[dbo].[Params_To_Table] (@AccessList, @delimiter)) THEN 'true' ELSE 'false' END AS [CanIEditVessel]
 
+      FOR XML PATH ('r0'), TYPE
+      ) as [Field]
   , ( SELECT
         (SELECT [CityName] +';'
         FROM [LabProtocols].[dbo].[Ent_Laboratories]
