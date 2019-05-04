44ca64fb9b990b7bcbe81c7b2f974acf98a140c5
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-05-04 12:50:52 +0700

    Query for json

---
+++ src/Query/Field_Response_AfterSave.sql
@@ -0,0 +1,264 @@
+IF ('@PARAM2@' = 'SaveVessel')
+BEGIN
+  IF '@Status@' ='' or '@Status@' = NULL
+        BEGIN
+    SET @Status='OK'
+  END
+    ELSE BEGIN
+    SET @Status=ltrim(rtrim('@Status@'))
+  END
+
+  BEGIN TRY
+
+	IF ('@CommissioningDate@'<>'' and '@CommissioningDate@'<>'/' and '@CommissioningDate@'<>N'Нет данных')
+		BEGIN
+    BEGIN TRY SET @CommissioningDate=CONVERT(date,'@CommissioningDate@',103) END TRY
+			BEGIN CATCH SELECT 'Ошибка в дате подачи заявления' END CATCH
+  END
+	ELSE
+		BEGIN
+    SET @CommissioningDate=NULL
+  END
+
+	IF ('@CertificationDate@'<>'' and '@CertificationDate@'<>'/' and '@CertificationDate@'<>N'Нет данных')
+		BEGIN
+    BEGIN TRY SET @CertificationDate=CONVERT(date,'@CertificationDate@',103) END TRY
+			BEGIN CATCH SELECT 'Ошибка в дате подачи заявления' END CATCH
+  END
+	ELSE
+		BEGIN
+    SET @CertificationDate=NULL
+  END
+
+	IF ('@LastCheckDate@'<>'' and '@LastCheckDate@'<>'/' and '@LastCheckDate@'<>N'Нет данных')
+		BEGIN
+    BEGIN TRY SET @LastCheckDate=CONVERT(date,'@LastCheckDate@',103) END TRY
+			BEGIN CATCH SELECT 'Ошибка в дате подачи заявления' END CATCH
+  END
+	ELSE
+		BEGIN
+    SET @LastCheckDate=NULL
+  END
+
+	IF ('@Score@'='2')
+		BEGIN
+    SET @DecommissioningDate=GETDATE()
+    SET @Status = 'Util'
+    SET @Condition = 'Выведена из эксплуатации ' + (SELECT [Condition]
+    FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+    WHERE CAST([ID] AS nvarchar(50)) = '@unid@')
+
+    SET @NewLastAutoCounterDate = dateadd(hour,4,getdate())
+
+    INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+      ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+    VALUES
+      (NewID(), '@unid@', 'Выведена из эксплуатации', NULL , @DecommissioningDate , @NewLastAutoCounterDate, '@UserName@')
+  END
+	ELSE
+		BEGIN
+    SET @DecommissioningDate=NULL
+    SET @Condition = ltrim(rtrim('@Condition@'))
+  END
+
+	IF ('@Condition@'<>'' and '@Serial@'<>'' and '@Location@'<>'')
+	BEGIN
+    IF ('@unid@' like '%unid%' or '@unid@'='')
+		BEGIN
+      SET @Serial = ( SELECT [Serial]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE [Serial] =ltrim(rtrim('@Serial@')) )
+      IF @Serial is null
+		  BEGIN
+        SET @NewVesselID = NewID()
+        SET @NewLastAutoCounterDate=dateadd(hour,4,getdate())
+
+        INSERT  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ INTO [LabProtocols].[dbo].[Ent_Lab_Entity]
+          ( [ID]
+          ,[Condition]
+          ,[Serial]
+          ,[Status]
+          ,[CommissioningDate]
+          ,[CertificationDate]
+          ,[LastCheckDate]
+          ,[Score]
+          ,[CommissioningCount]
+          ,[LastCheckCount]
+          ,[Location]
+          ,[LocationCode]
+          ,[LabCode]
+          ,[LastChangedBy]
+          ,[VesselType]
+          )
+        VALUES
+          (@NewVesselID
+						 , @Condition
+						 , ltrim(rtrim('@Serial@'))
+						 , @Status
+						 , @CommissioningDate
+						 , @CertificationDate
+						 , @LastCheckDate
+						, ltrim(rtrim('@Score@')) 
+						, 0
+						, 0
+						, '@Location@', (SELECT TOP 1
+              [Code]
+            FROM [RUWS002].[AllVostok].[dbo].[Ref_Location] as a
+            WHERE a.[Name] = '@Location@')
+						, (SELECT TOP 1
+              [LabCode]
+            FROM [LabProtocols].[dbo].[Ent_Laboratories]
+            WHERE [CityName] = '@Location@')
+
+						, '@UserName@'
+						, '@VesselType@'
+						)
+
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), @NewVesselID, 'Создание записи', NULL , @Condition+'-'+'@Location@', @NewLastAutoCounterDate, '@UserName@')
+
+        /*Создание истории для месячного накопления для графиков*/
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), @NewVesselID, 'Поджиги', CAST( YEAR( @NewLastAutoCounterDate ) AS CHAR(4) ) + '-'+ LEFT( DATENAME ( MONTH, @NewLastAutoCounterDate ),3 ), 0, @NewLastAutoCounterDate, '@UserName@')
+
+        IF (@CertificationDate IS NOT NULL)
+			BEGIN
+          INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+            ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+          VALUES
+            (NewID(), @NewVesselID, 'Фиксация поджигов перед отправкой в ЦСМ', NULL , '0', @NewLastAutoCounterDate, '@UserName@')
+        END
+
+      END
+		  ELSE
+		  BEGIN
+        SELECT 'Сосуд уже существует'
+      END
+    END
+		ELSE
+		BEGIN
+      SET @NewLastAutoCounterDate = dateadd(hour,4,getdate())
+      /*Сброс счётчика после проверки сосуда*/
+      SET @LastCheckDateCheck = (SELECT [LastCheckDate]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE CAST([ID] AS nvarchar(50))='@unid@')
+
+      IF @LastCheckDateCheck <> @LastCheckDate
+			BEGIN
+        UPDATE [LabProtocols].[dbo].[Ent_Lab_Entity]
+				SET [LastCheckCount]=0
+			  WHERE CAST([ID] AS nvarchar(50))='@unid@'
+
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), '@unid@', 'Периодическая проверка', NULL , 'с ' + CAST(@LastCheckDateCheck AS nvarchar(10) ) + ' по ' + CAST(@LastCheckDate AS nvarchar(10) ) , @NewLastAutoCounterDate, '@UserName@')
+      END
+
+      SET @LocationOld = (SELECT [Location]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE CAST([ID] AS nvarchar(50))='@unid@')
+
+      IF @LocationOld <> '@Location@'
+			BEGIN
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), '@unid@', 'Смена локации', NULL , 'из ' + @LocationOld + ' в ' + '@Location@' , @NewLastAutoCounterDate, '@UserName@')
+      END
+
+      SET @StatusOld = (SELECT [Condition]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE CAST([ID] AS nvarchar(50)) = '@unid@')
+
+      IF @StatusOld <> @Condition
+			BEGIN
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), '@unid@', 'Смена статуса', NULL , @Condition , @NewLastAutoCounterDate, '@UserName@')
+      END
+
+
+      SET @ScoreOld = (SELECT [Score]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE CAST([ID] AS nvarchar(50)) = '@unid@')
+
+      IF @ScoreOld <> ltrim(rtrim('@Score@')) 
+			BEGIN
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), '@unid@', 'Изменена оценка', NULL , ltrim(rtrim('@Score@')), @NewLastAutoCounterDate, '@UserName@')
+      END
+
+      SET @CertificationDateOld = (SELECT [CertificationDate]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+      WHERE CAST([ID] AS nvarchar(36)) = '@unid@')
+
+      IF @CertificationDateOld <> @CertificationDate
+			BEGIN
+        INSERT INTO  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          ([ID],[EntityID],[Item],[ItemGroup] ,[ItemVal] ,[Created],[CreatedBy])
+        VALUES
+          (NewID(), '@unid@', 'Фиксация поджигов перед отправкой в ЦСМ', NULL , (SELECT CAST([CommissioningCount] AS nvarchar(10))
+            FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+            WHERE CAST([ID] AS nvarchar(36)) = '@unid@'), @NewLastAutoCounterDate, '@UserName@')
+      END
+
+      UPDATE  /*POST SERVICE LAB VesselMonitoringBackend @PARAM2@, @UserName@, @GetDate@*/ [LabProtocols].[dbo].[Ent_Lab_Entity]
+			SET [Condition] = @Condition
+				,[Serial] = ltrim(rtrim('@Serial@'))
+				,[Status] = @Status
+				,[CommissioningDate] = @CommissioningDate
+				,[CertificationDate] = @CertificationDate
+				,[LastCheckDate] = @LastCheckDate
+				,[Score] = ltrim(rtrim('@Score@')) 
+
+				,[DecommissioningDate] = @DecommissioningDate
+				,[Location] = '@Location@'
+				,[LocationCode] = (SELECT TOP 1
+        [Code]
+      FROM [RUWS002].[AllVostok].[dbo].[Ref_Location] as a
+      WHERE a.[Name] = '@Location@')
+				,[LabCode] = (SELECT TOP 1
+        [LabCode]
+      FROM [LabProtocols].[dbo].[Ent_Laboratories]
+      WHERE [CityName] = '@Location@')
+ 				,[LastChangedBy] = '@UserName@'
+				,[VesselType] = '@VesselType@'
+				WHERE CAST([ID] AS nvarchar(50))='@unid@'
+      SET @NewVesselID = '@unid@'
+    END
+  END
+	ELSE 
+	BEGIN
+    SELECT char(10)+'Не все обязательные поля заполнены'
+  END
+    
+SELECT [LabProtocols].dbo.qfn_XmlToJson 
+	((
+	  SELECT
+      @NewVesselID as unid
+	  , (SELECT
+        ELEH.[Item]
+		  , ELEH.[ItemVal]
+		 , ELEH.[CreatedBy]
+	   , convert(char(19),ELEH.[Created],120)  AS [Created]
+      FROM [LabProtocols].[dbo].[Ent_Lab_Entity_History] as ELEH
+      WHERE CAST(ELEH.[EntityID] AS nvarchar(36)) = @NewVesselID AND ELEH.[ItemGroup] IS NULL
+        AND ELEH.[Created] = @NewLastAutoCounterDate
+      ORDER BY Created DESC
+      FOR xml path('HistoryPart'),root('HistoryPart'),type)
+    FOR xml path('vd'),type
+	  ))
+    
+END TRY
+BEGIN CATCH
+	SELECT '@PARAM2@ отработал с ошибкой: '+ERROR_MESSAGE()+', Номер строки: '+CAST(ERROR_LINE() AS nvarchar(max));
+END CATCH
+END
---
+++ src/Query/Table_Load&Response_Vessels.sql
@@ -0,0 +1,99 @@
+IF ('@PARAM2@' = 'Vessels_GetData')
+BEGIN
+  BEGIN TRY
+
+	IF ('@unid@' <> '@'+'unid'+'@' and '@unid@' <> '')
+	BEGIN
+    SET @isSingleResults = 1
+  END
+	ELSE
+	BEGIN
+    SET @isSingleResults = 0
+  END
+
+	SET @AccessList =(SELECT [AccessQuery]
+  FROM [NKReports].[dbo].[DB_Settings_ACL]
+  WHERE [UserName]='@UserName@' AND [TableName]='LabVesselMonitoring')
+	SET @delimiter  = ','
+
+	DECLARE @t_Location as Table([Location] nvarchar(max),
+    [LocationCode] int,
+    [Condition] nvarchar(max) )
+	  INSERT INTO @t_Location
+  SELECT DISTINCT [Location], [LocationCode] , [Condition]
+  FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+  WHERE 
+	   ( @isSingleResults=0 AND [LabCode] in ( SELECT nstr
+    FROM [NKReports].[dbo].[Params_To_Table] ( @AccessList,@delimiter ) ) )
+    OR
+    (@isSingleResults=1 AND [LabCode] in ( SELECT nstr
+    FROM [NKReports].[dbo].[Params_To_Table] ( @AccessList,@delimiter ) ) AND CAST(id AS nvarchar(50)) ='@unid@' )
+
+  GROUP BY [Location],[LocationCode],LabCode,[Condition]
+
+	DECLARE @t_LocationUnique as Table([Location] nvarchar(max),
+    [LocationCode] int,
+    LabCode nvarchar(5))
+	  INSERT INTO @t_LocationUnique
+  SELECT DISTINCT [Location] , [LocationCode] , LabCode
+  FROM [LabProtocols].[dbo].[Ent_Lab_Entity]
+  WHERE 
+		( @isSingleResults=0 AND [LabCode] in ( SELECT nstr
+    FROM [NKReports].[dbo].[Params_To_Table] ( @AccessList,@delimiter ) ) )
+    OR
+    ( @isSingleResults=1 AND [LabCode] in ( SELECT nstr
+    FROM [NKReports].[dbo].[Params_To_Table] ( @AccessList,@delimiter ) ) AND CAST(id AS nvarchar(50))='@unid@' )
+  GROUP BY [Location],[LocationCode],LabCode
+
+	SELECT [LabProtocols].dbo.qfn_XmlToJson 
+	((
+	  SELECT
+      t1.[Location]
+		  , (SELECT t2.[Condition]
+			 , (SELECT ELE.[ID]
+			   	, ELE.[Serial]
+				, CASE WHEN ELE.[LastCheckCount] > 5000 THEN 'Требуется проверка' ELSE 
+		 		 CASE WHEN ( datediff(d, ELE.[CertificationDate], getdate() )>=365 AND ELE.[Status]<>'Util' ) THEN 'Требуется испытание' ELSE ELE.[Status] END END AS [Status]
+				, CASE WHEN ELE.[CommissioningDate] IS NULL THEN 'Нет данных' ELSE CONVERT(nchar(10),ELE.[CommissioningDate],103) END AS CommissioningDate
+				, CASE WHEN ELE.[CertificationDate] IS NULL THEN 'Нет данных' ELSE CONVERT(nchar(10),ELE.[CertificationDate],103) END AS CertificationDate
+				, CASE WHEN ELE.[LastCheckDate] IS NULL THEN 'Нет данных' ELSE CONVERT(nchar(10),ELE.[LastCheckDate],103) END AS LastCheckDate
+				, ELE.[Score]
+				, ELE.[CommissioningCount]
+				, ELE.[LastCheckCount]
+			   , (SELECT TOP 1
+            ItemVal
+          FROM [LabProtocols].[dbo].[Ent_Lab_Entity_History]
+          WHERE EntityID = ELE.ID AND Item = N'Фиксация поджигов перед отправкой в ЦСМ'
+          ORDER BY Created DESC) AS [CertificationCount]
+				, CASE WHEN ELE.[LastAutoCounterDate] IS NULL THEN 'Нет данных' ELSE CONVERT(nchar(19),ELE.[LastAutoCounterDate],120) END AS LastAutoCounterDate
+				, ELE.[LastChangedBy]
+			   	, 'false' AS [onAction]
+        FROM [LabProtocols].[dbo].[Ent_Lab_Entity] AS ELE
+
+        WHERE 
+		( @isSingleResults=0 AND ELE.[Location] = t1.[Location]
+          AND ELE.[LocationCode] = t1.[LocationCode]
+          AND ELE.[Condition] = t2.[Condition] )
+          OR
+          ( @isSingleResults=1 AND ELE.[Location] = t1.[Location]
+          AND ELE.[LocationCode] = t1.[LocationCode]
+          AND ELE.[Condition] = t2.[Condition] AND CAST(id AS nvarchar(50))='@unid@' )
+        FOR XML PATH('VD'), TYPE 
+
+			  ) AS [VesselDetails]
+
+      FROM @t_Location AS t2
+      where t2.[Location] = t1.[Location]
+        AND t2.[LocationCode] = t1.[LocationCode]
+      FOR XML PATH('kk'), TYPE ) AS [ConditionDetails]
+
+    FROM @t_LocationUnique AS t1
+    FOR XML PATH('k'), TYPE 
+
+	 ))
+
+  END TRY
+  BEGIN CATCH
+	SELECT '@PARAM2@ отработал с ошибкой: '+ERROR_MESSAGE()+', Номер строки: '+CAST(ERROR_LINE() AS nvarchar(max));
+  END CATCH
+END
