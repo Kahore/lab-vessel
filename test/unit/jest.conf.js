27ef52f259613d0d8e88b0495ea0d49b32fe6c95
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-05-04 11:38:23 +0700

    config eslint changes

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,26 +1,17 @@
-const path = require('path')
-
+const path = require('path');
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
-  moduleFileExtensions: [
-    'js',
-    'json',
-    'vue'
-  ],
+  moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
-  collectCoverageFrom: [
-    'src/**/*.{js,vue}',
-    '!src/main.js',
-    '!**/node_modules/**'
-  ]
-}
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js', '!**/node_modules/**'],
+};
